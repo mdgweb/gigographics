859e61f38be58f5e7d141b82dbc2b1be10409aed
Ciack404 <glorfindel83@gmail.com>
2013-01-19 16:32:41 +0000

    multiple marker

--- static/gigographics.js
+++ static/gigographics.js
@@ -8,7 +8,7 @@ window.onload = function() {
     var latLng = new google.maps.LatLng(59.32893,18.06491);
 
     var myOptions = {
-        panControl: false,
+/*        panControl: false,
         zoomControl: true,
         mapTypeControl: false,
         scaleControl: false,
@@ -17,19 +17,38 @@ window.onload = function() {
         draggable: true,
         disableDoubleClickZoom: true,     //disable zooming
         scrollwheel: true,
-        zoom: 20,
+*/        zoom: 4,
         center: latLng,
         mapTypeId: google.maps.MapTypeId.ROADMAP //   ROADMAP; SATELLITE; HYBRID; TERRAIN;
     };
 
     $.gigographics.map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 
-    var params= {
-        'title' : 'Stockholm',
+    var locations = [
+                     ['Bondi Beach', -33.890542, 151.274856, 4],
+                     ['Coogee Beach', -33.923036, 151.259052, 5],
+                     ['Cronulla Beach', -34.028249, 151.157507, 3],
+                     ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+                     ['Maroubra Beach', -33.950198, 151.259302, 1]
+                     ];
+
+    add_markers(locations);
+
+/*    var params= {
+        'title' : 'Spotify',
         'lat' : 59.32893,
         'lng' : 18.06491,
         'content' : 'Stockholm city'
     }
     add_marker(params);
-
+    
+    params= {
+        'title' : 'Dropkick',
+        'lat' : 55.8550553,
+        'lng' : -4.2369184,
+        'content' : 'Dropkick gig'
+    }
+    
+    add_marker(params);
+*/
 }
--- static/helper.js
+++ static/helper.js
@@ -10,7 +10,7 @@ icon: image
 //for animation marker drop
 animation: google.maps.Animation.DROP
 
-*/
+
 function add_marker(params) {
     var markerlatlng = new google.maps.LatLng(params.lat, params.lng);
 
@@ -21,12 +21,13 @@ function add_marker(params) {
 
     marker.setMap($.gigographics.map);
 
-    /*
-     INFO Bubble
-     */
-
+    var explanation= '<div class="concert_location">' +
+                            '<h4>' + params.title + '</h4>' +
+                            params.content +
+                    '</div>'
+    
     myInfoWindowOptions = {
-        content: '<div class="concert_location"><h4>' + params.title + '</h4>' + params.content + '</div>',
+        content: explanation,
         maxWidth: 275
     };
 
@@ -42,4 +43,30 @@ function add_marker(params) {
 
     infoWindow.open($.gigographics.map, marker);
     
+}
+*/	
+
+function add_markers(locations) {
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: $.gigographics.map
+        });
+        var explanation= '<div class="concert_location">' +
+                        '<h4>' + locations[i][0] + '</h4>' +
+                        'LAT: ' + locations[i][1] +
+                        'LONG: ' + locations[i][2] +
+                        '</div>'
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infowindow.setContent(explanation);
+                infowindow.open($.gigographics.map, marker);
+            }
+        })(marker, i));
+    }
 }
